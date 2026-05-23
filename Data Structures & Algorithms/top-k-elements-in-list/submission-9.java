28578b531c1591ab47437eedb56b64789016afc8
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-05-23 23:06:45 +0530

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Step 1: Count total frequencies using a HashMap
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)-> a[1]-b[1]);
+
+        for(int key: freqMap.keySet()){
+            minHeap.add(new int []{key, freqMap.get(key)});
+
+            if(minHeap.size() > k) minHeap.poll();
+        }
+
+        // Step 4: Simply pull the numbers from the first k elements!
+        int[] result = new int[k];
+        int i = 0;
+        while(minHeap.size() > 0){
+            int [] arr = minHeap.poll();
+            result[i++] = arr[0];
+        }
+        
+        return result;
+    }
+}
+
+// class Solution {
+//     static class Pair {
+//         int num;
+//         int freq;
+
+//         Pair(int num, int freq) {
+//             this.num = num;
+//             this.freq = freq;
+//         }
+
+//         // Add this getter so Pair::getFreq works!
+//         public int getFreq() {
+//             return this.freq;
+//         }
+//     }
+
+//     public int[] topKFrequent(int[] nums, int k) {
+//         // Step 1: Count total frequencies using a HashMap
+//         Map<Integer, Integer> freqMap = new HashMap<>();
+//         for (int num : nums) {
+//             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+//         }
+
+//         // Step 2: Build a list of ALL Pairs
+//         List<Pair> pairList = new ArrayList<>();
+//         for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+//             pairList.add(new Pair(entry.getKey(), entry.getValue()));
+//         }
+
+//         pairList.sort((p1, p2) -> Integer.compare(p2.freq, p1.freq));
+//         // pairList.sort(Comparator.comparing(Pair::getFreq).reversed());
+
+//         // Step 4: Simply pull the numbers from the first k elements!
+//         int[] result = new int[k];
+//         for (int i = 0; i < k; i++) {
+//             result[i] = pairList.get(i).num;
+//         }
+        
+//         return result;
+//     }
+// }
