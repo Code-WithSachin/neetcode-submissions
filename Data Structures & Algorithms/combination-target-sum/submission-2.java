2c48cf981953bab8b7b983509a969d1d461e7bb9
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-06-11 01:19:15 +0530

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+   public static void recur(int index, int []arr, List<Integer>helper,  List<List<Integer>>ans, int target){
+       if(target<0||index==arr.length) return;
+       if(target==0){
+           ans.add(new ArrayList<>(helper));
+           return;
+       }
+       
+       helper.add(arr[index]);
+       recur(index,arr, helper, ans, target-arr[index]);
+       helper.remove(helper.size()-1);
+       recur(index+1,arr, helper, ans, target);
+   }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>>ans=new ArrayList<>();
+        recur(0, candidates,new ArrayList<>(), ans, target);
+        return ans;
+    }
+}
