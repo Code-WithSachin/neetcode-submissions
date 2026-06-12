e08eb6ef01981f100ddd4155c534bb6d702afc50
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-06-12 22:54:52 +0530

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(char curr[]){
+        int balance = 0;
+        for(char ch: curr){
+            if(ch == '(') balance++;
+            else balance--;
+
+            if(balance < 0) return false;
+        }
+
+        return balance == 0;
+    }
+    public void generate(int index, char[] curr, List<String> ans,
+    int openCount, int closeCount, int maxAllowed){
+
+        if(openCount > maxAllowed || closeCount > maxAllowed || closeCount > openCount) return;
+
+        if(index == curr.length){
+            ans.add(new String(curr));
+            return;
+        }
+
+        curr[index] = '(';
+        generate(index+1, curr, ans, openCount+1, closeCount, maxAllowed);
+
+        curr[index] = ')';
+        generate(index+1, curr, ans, openCount, closeCount+1, maxAllowed);
+    }
+    public List<String> generateParenthesis(int n) {
+        char [] curr = new char[2*n];
+        List<String> ans = new ArrayList<>();
+        generate(0, curr, ans, 0, 0, n);
+        return ans;
+    }
+}
