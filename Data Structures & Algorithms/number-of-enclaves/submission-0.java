1e8125646c39bbb50d4e8608c8f339f850a02c18
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-06-14 02:06:04 +0530

    Add: number-of-enclaves - submission-0

---
+++ Data Structures & Algorithms/number-of-enclaves/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    public void check(int indexI, int indexJ, int[][] grid){
+        int n = grid.length, m = grid[0].length;
+        if(indexI <0 || indexJ < 0 || indexI >= n || indexJ >= m || grid[indexI][indexJ] == 0) return;
+
+        int directionX [] = {1, 0, -1, 0};
+        int directionY [] = {0, -1, 0, 1};
+
+        grid[indexI][indexJ] = 0;
+
+        for(int i = 0; i<4; i++){
+             check(indexI+directionX[i], indexJ+directionY[i], grid);
+        }
+    }
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<m; j++){
+                if(i == 0 || i == n-1 || j == 0 || j == m-1 && grid[i][j]==1){
+                    check(i, j, grid);
+                }
+            }
+        }
+
+        int countOfOnes = 0;
+        for(int i = 0; i<n; i++){
+            for(int j =0; j<m; j++){
+                if(grid[i][j]==1){
+                    countOfOnes++;
+                }
+            }
+        }
+        return countOfOnes;
+    }
+}
