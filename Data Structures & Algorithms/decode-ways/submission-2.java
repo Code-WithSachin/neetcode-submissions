839fcdb345e92ccfdff4e8ebb2614e7da38038fb
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-06-17 23:54:45 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodingsHelper(int index, String s, int [] memo){
+        if(index == s.length()) return 1;
+        if(s.charAt(index) == '0') return 0;
+
+        if(memo[index] != 0) return memo[index];
+
+        int result = numDecodingsHelper(index+1, s, memo);
+        if (index + 2 <= s.length() && Integer.parseInt(s.substring(index, index + 2)) < 27){
+            result+=numDecodingsHelper(index+2, s, memo);
+        }
+
+        return memo[index] = result;
+    }
+    public int numDecodings(String s) {
+        int [] memo = new int[s.length()];
+        return numDecodingsHelper(0, s, memo);
+    }
+}
