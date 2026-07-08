881e10b9b1459ffa598e05623a5d038588efdcee
Code-WithSachin <109336389+Code-WithSachin@users.noreply.github.com>
2026-07-08 23:49:11 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
+        }
+
+        // Max Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int freq : map.values()) {
+            pq.offer(freq);
+        }
+
+        // {remainingFrequency, availableTime}
+        Queue<int[]> queue = new LinkedList<>();
+
+        int time = 0;
+
+        while (!pq.isEmpty() || !queue.isEmpty()) {
+
+            time++;
+
+            if (!pq.isEmpty()) {
+
+                int freq = pq.poll();
+                freq--;
+
+                if (freq > 0) {
+                    queue.offer(new int[]{freq, time + n});
+                }
+            }
+
+            if (!queue.isEmpty() && queue.peek()[1] == time) {
+                pq.offer(queue.poll()[0]);
+            }
+        }
+
+        return time;
+    }
+}
